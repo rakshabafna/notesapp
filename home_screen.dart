bb6df3a2d44e515e69dc341e0e3179f60fdfa2e0
rakshabafna <rakshabafna2006@gmail.com>
2024-08-24 20:25:01 +0530

    Add files via upload

--- home_screen.dart
+++ home_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:notes_app/models/notes_model.dart';
 import 'package:notes_app/screens/create_notes.dart';
 import 'package:notes_app/screens/widgets/note_card.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -18,7 +20,44 @@ class _HomeScreenState extends State<HomeScreen> {
     Colors.green[200]!,
     Colors.orange[200]!,
     Colors.blue[200]!,
-    ];
+  ];
+  @override
+  void initState() {
+    super.initState();
+    loading();
+  }
+
+  void loading() async {
+    final prefs = await SharedPreferences.getInstance();
+    final notesJson = prefs.getString('notes') ?? '[]';
+    final List<Map<String, String>> notesList = List<Map<String, String>>.from(
+        jsonDecode(notesJson).map((item) => Map<String, String>.from(item))
+    );
+    setState(() {
+      notes = notesList.map((noteMap) => Note.fromMap(noteMap)).toList();
+    });
+  }
+
+  void saveN() async {
+    final prefs = await SharedPreferences.getInstance();
+    final notesJson = jsonEncode(notes.map((note) => note.toMap()).toList());
+    await prefs.setString('notes', notesJson);
+  }
+
+  void addN(Note note) {
+    setState(() {
+      notes.add(note);
+      saveN();
+    });
+  }
+
+  void deleteN(int index) {
+    setState(() {
+      notes.removeAt(index);
+      saveN();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,29 +69,30 @@ class _HomeScreenState extends State<HomeScreen> {
           fontSize: 40.0,
         ),
       )),
-      body:  Column(
+      body: Column(
         children: [
           Center(
             child: Image.asset(
-            'assets/images/notes.png',
-            fit:BoxFit.cover,
+              'assets/images/notes.png',
+              fit: BoxFit.cover,
             ),
           ),
           Expanded(
-            child:ListView.builder(
-          itemCount: notes.length,
-          itemBuilder: (context, index) {
-            return NoteCard(
-                note: notes[index], index: index, onNoteDeleted: onNoteDeleted, color: colors[index % colors.length],);
-          }
-          ),
+            child: ListView.builder(
+                itemCount: notes.length,
+                itemBuilder: (context, index) {
+                  return NoteCard(
+                    note: notes[index],
+                    index: index,
+                    onNoteDeleted: onNoteDeleted,
+                    color: colors[index % colors.length],
+                  );
+                }),
           ),
         ],
       ),
-    
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-        
           Navigator.of(context).push(MaterialPageRoute(
               builder: (context) => CreateNotes(
                     onNewNoteCreated: onNewNoteCreated,
@@ -74,3 +114,5 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {});
   }
 }
+
+class Notes {}
--- notes_model.dart
+++ notes_model.dart
@@ -3,4 +3,18 @@ class Note {
   final String body;
 
   Note({required this.title, required this.body});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'body': body,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      title: map['title'],
+      body: map['body'],
+    );
+  }
 }
